01137b5c2ac54348e21b6d5e30d15db1d3f34411
amir <amirhossein.z101.ahmadi@gmail.com>
2024-06-27 04:47:22 -0700

    ppp

---
+++ js/app.js
@@ -0,0 +1,3 @@
+const ul = document.getElementById("qpw").classList.toggle("qp");
+const button = document.querySelector("logw").classList.add("log");
+const p = document.getElementById("informationw").classList.toggle("information");
